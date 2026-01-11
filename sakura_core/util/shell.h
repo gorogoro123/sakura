646c8d0ecae7b25afe3918d6cd9d04640bfd385f
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2026-01-11 21:26:45 +0900

    remove OpenHtmlHelp

--- sakura_core/util/shell.h
+++ sakura_core/util/shell.h
@@ -22,7 +22,6 @@ BOOL SelectDir(HWND hWnd, const std::wstring& title, const std::filesystem::path
 
 bool		ResolveShortcutLink(HWND hWnd, const std::filesystem::path& linkFile, std::span<WCHAR> szPath);	/* ショートカット(.lnk)の解決 */
 
-HWND OpenHtmlHelp( HWND hWnd, LPCWSTR szFile, UINT uCmd, DWORD_PTR data,bool msgflag = true);
 DWORD NetConnect ( const WCHAR strNetWorkPass[] );
 
 /* ヘルプの目次を表示 */
